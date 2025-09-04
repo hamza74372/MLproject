f4467df5cf562461a64c9abb92058b48ab3cb802
hamza74372 <hamzashakil497@gmail.com>
2025-09-04 14:24:38 +0500

    Another commit

--- src/logger.py
+++ src/logger.py
@@ -2,28 +2,16 @@ import logging
 import os
 from datetime import datetime
 
-# Create logs folder if not exists
-LOG_DIR = os.path.join(os.getcwd(), "logs")
-os.makedirs(LOG_DIR, exist_ok=True)
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+os.makedirs(logs_path,exist_ok=True)
 
-# Log file name with timestamp
-LOG_FILE = os.path.join(LOG_DIR, f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log")
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
 
-# Create logger
-logger = logging.getLogger("MLProjectLogger")
-logger.setLevel(logging.INFO)
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
 
-# Formatter
-formatter = logging.Formatter("[%(asctime)s] %(levelname)s - %(message)s")
 
-# File handler
-file_handler = logging.FileHandler(LOG_FILE)
-file_handler.setFormatter(formatter)
-logger.addHandler(file_handler)
-
-# Optional: also print to console
-console_handler = logging.StreamHandler()
-console_handler.setFormatter(formatter)
-logger.addHandler(console_handler)
-
-logger.info(f"Logger initialized. Log file: {LOG_FILE}")
+)
